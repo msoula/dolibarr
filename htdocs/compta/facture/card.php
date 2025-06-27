4aa089849276fb7f71d6bf5bf60e4848f251de6c
rid <msoula@gmx.com>
2025-06-27 13:10:35 +0200

    Autoset date value for invoice form

--- htdocs/compta/facture/card.php
+++ htdocs/compta/facture/card.php
@@ -3974,7 +3974,17 @@ if ($action == 'create') {
 			print '</td></tr>';
 		}
 
-		$newdateinvoice = dol_mktime(0, 0, 0, GETPOSTINT('remonth'), GETPOSTINT('reday'), GETPOSTINT('reyear'), 'tzserver');
+		// u2042: autoset date value
+		$newdatemonth = GETPOSTINT('remonth');
+		$newdateday   = GETPOSTINT('reday');
+		$newdateyear  = GETPOSTINT('reyear');
+		if (empty($newdate) && empty($newdateday) && empty($newdateyear)) {
+			$tmp = dol_getdate(dol_now('tzuser'));
+			$newdatemonth = $tmp['mon'];
+			$newdateday   = $tmp['mday'];
+			$newdateyear  = $tmp['year'];
+		}
+		$newdateinvoice = dol_mktime(0, 0, 0, $newdatemonth, $newdateday, $newdateyear, 'tzserver');
 		$date_pointoftax = dol_mktime(0, 0, 0, GETPOSTINT('date_pointoftaxmonth'), GETPOSTINT('date_pointoftaxday'), GETPOSTINT('date_pointoftaxyear'), 'tzserver');
 
 		// Date invoice
